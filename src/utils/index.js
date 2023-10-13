537037c4fb7a098b8eb77d90a9cd5c5e14560140
soobin28 <49634513+soobin28@users.noreply.github.com>
2023-10-13 14:20:28 +0900

    쿠키 데이터 암호화 처리
    쿠키 데이터 저장 암호화 처리 / 쿠키 데이터 로드 시 복호화 처리 추가

--- src/utils/index.js
+++ src/utils/index.js
@@ -15,21 +15,23 @@ export const getCookie = (name, options = null) => {
   const value = window.document.cookie.match(
     '(^|;) ?' + name + '=([^;]*)(;|$)'
   );
-  return value ? value[2] : null;
+  return value ? decodeURIComponent(value[2]) : null;
 };
 // setCookie
 export const setCookie = (name, value, expires = 1, callback = false) => {
   var date = new Date();
   date.setTime(date.getTime() + expires * 1000 * 60 * 60 * 24);
-  window.document.cookie = `${name}=${value};expires=${date.toUTCString()}; path=/`;
+  window.document.cookie = `${name}=${encodeURIComponent(value)};expires=${date.toUTCString()}; path=/`;
   if (callback) callback();
 };
 
 // clearCookie
 // export const deleteCookie = (name, { path, domain }) => {
 export const deleteCookie = (name) => {
+  var date = new Date();
+  date.setTime(date.getTime() - 1000);
   if (getCookie(name)) {
-    window.document.cookie = `${name}=;path=/;domain=${PUBLIC_URL};expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+    window.document.cookie = `${name}=;path=/;domain=http://fchat.codebrewing.org; expires=${date.toUTCString()};`;
     // window.document.cookie =
     //   name +
     //   '=' +
